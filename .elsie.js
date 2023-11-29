ed97586c0e96944cc563a97edd7cc6b288685046
Bruce Denham <bdenham@adobe.com>
2023-11-29 12:14:09 -0600

    test: github code linking

---
+++ .elsie.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Base',
+  components: [
+    {
+      id: 'Components',
+      root: './src/components',
+      importAliasRoot: '@adobe/elsie/components',
+      cssPrefix: 'elsie',
+      default: true,
+    },
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
+const path = require('path');
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias['@adobe/elsie/components'] = path.join(__dirname, 'node_modules/@adobe/elsie/src/components');
+
+    // config.module.rules.push({
+    //   test: /\.tsx?$/,
+    //   use: 'ts-loader',
+    //   exclude: /node_modules\/(?!(@adobe\/elsie)\/).*/,
+    // });
+
+    // // If there's a webpack configuration in the '@adobe/elsie' package, apply it
+    // const elsieWebpackConfig = require('@adobe/elsie/config/webpack');
+    // if (typeof elsieWebpackConfig === 'function') {
+    //   config = elsieWebpackConfig(config, { buildId, dev, isServer, defaultLoaders, webpack });
+    // }
+
+    if (config.mode !== undefined) {
+      console.log(`webpack mode ${config.mode}`);
+    }
+
+    return config;
+  },
+});
